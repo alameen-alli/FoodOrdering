df47953450bfedbc226ed5e3861a24892c45f987
alameen-alli <allialameen8739@gmail.com>
2024-08-02 19:40:51 +0100

    feat: update order status

--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -1,12 +1,12 @@
 import { FlatList } from 'react-native';
-import orders from '../../../../assets/data/orders';
 import { Stack } from 'expo-router';
 import OrderItemListItem from '@/src/components/OrderListItem';
+import orders from '@/assets/data/orders';
 
 export default function OrdersScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Orders' }} />
+      {/* <Stack.Screen options={{ title: 'Orders' }} /> */}
       <FlatList
         data={orders}
         contentContainerStyle={{ gap: 10, padding: 10 }}
